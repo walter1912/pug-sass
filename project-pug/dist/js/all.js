d72046bfdada0bd4474b1d4230a9339dbcee02cd
walter1912 <thaiho2k@gmail.com>
2023-07-27 02:56:14 +0700

    done header

--- project-pug/dist/js/all.js
+++ project-pug/dist/js/all.js
@@ -1,1 +1,9 @@
 console.log("hello Pug");
+
+function toogleSearch(e) {
+  console.log("this: ", e);
+  let $search__dropdown = document.querySelector(".search__dropdown");
+  if ($search__dropdown.style.display === "none")
+    $search__dropdown.style.display = "block";
+  else $search__dropdown.style.display = "none";
+}
